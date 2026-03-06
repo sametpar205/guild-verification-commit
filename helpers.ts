f93f137d8f70dcf370c79718d216b7585b476aaf
sametpar205 <sametpar250@gmail.com>
2026-03-06 04:29:20 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T01:29:07.766Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T01:29:21.774Z
+Commit: Patch API endpoint error handling
