0db68382f29cce6591f2a439f91e594515c320c7
sametpar205 <sametpar250@gmail.com>
2026-03-06 04:28:13 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T01:28:11.250Z
-Commit: Add multi-language support
+Timestamp: 2026-03-06T01:28:14.863Z
+Commit: Resolve null pointer exception in user service
