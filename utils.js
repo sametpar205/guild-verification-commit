1f21fbe4f6d1d213c5be59dc41d3fcaac3b3093d
sametpar205 <sametpar250@gmail.com>
2026-03-06 04:28:45 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T01:28:43.079Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T01:28:47.562Z
+Commit: Resolve null pointer exception in user service
