b2990b59f67dc021feace3b90a0b80b3de55a906
sametpar205 <sametpar250@gmail.com>
2026-03-06 04:29:42 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T01:29:25.307Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T01:29:43.978Z
+Commit: Patch API endpoint error handling
